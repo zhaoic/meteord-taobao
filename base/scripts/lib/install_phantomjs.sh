399acd2c88ec345576b9764e5938e5fbece9c71e
zhaoic <zhaoic@163.com>
2016-04-12 22:21:20 +0800

    try to solve npm and fibers problems

--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -6,7 +6,9 @@ PHANTOMJS_VERSION=1.9.8
 PHANTOMJS_TAR_FILE=phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 
 cd /usr/local/share/
-curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+# curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+
+wget http://7xs3j2.com1.z0.glb.clouddn.com/meteord/phantomjs-1.9.8-linux-x86_64.tar.bz2
 tar xjf $PHANTOMJS_TAR_FILE
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
