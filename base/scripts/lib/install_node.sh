b9d41f2a7b406e3b2814bd2f6a43a29692bb5ec5
root <root@ip-172-31-26-127.us-west-2.compute.internal>
2016-08-04 15:40:36 +0000

    change npm source from taobao to origin

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -9,8 +9,8 @@ NODE_ARCH=x64
 NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
 
 cd /tmp
-# curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
-curl -O -L http://npm.taobao.org/mirrors/node/v${NODE_VERSION}/${NODE_DIST}.tar.gz
+curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
+# curl -O -L http://npm.taobao.org/mirrors/node/v${NODE_VERSION}/${NODE_DIST}.tar.gz
 tar xvzf ${NODE_DIST}.tar.gz
 rm -rf /opt/nodejs
 mv ${NODE_DIST} /opt/nodejs
@@ -19,7 +19,7 @@ ln -sf /opt/nodejs/bin/node /usr/bin/node
 ln -sf /opt/nodejs/bin/npm /usr/bin/npm
 
 echo "=> [**]Install npm@3"
-npm config set registry https://registry.npm.taobao.org
-npm config set disturl https://npm.taobao.org/dist
+#npm config set registry https://registry.npm.taobao.org
+#npm config set disturl https://npm.taobao.org/dist
 npm install --global npm@3
 echo "=> [**]Install npm@3 done"
--- base/scripts/run_app.sh
+++ base/scripts/run_app.sh
@@ -1,7 +1,7 @@
 set -e
 
-npm config set registry https://registry.npm.taobao.org
-npm config set disturl https://npm.taobao.org/dist
+#npm config set registry https://registry.npm.taobao.org
+#npm config set disturl https://npm.taobao.org/dist
 echo "=> Prepare run app."
 
 if [ -d /bundle ]; then
