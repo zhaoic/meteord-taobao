19ed7840d9fa50fd1568e4d9ef848cbac52271b0
zhaoic <zhaoic@163.com>
2016-09-21 10:41:33 +0800

    debug main.js

--- base/scripts/run_app.sh
+++ base/scripts/run_app.sh
@@ -45,14 +45,15 @@ set -e
 #   fi
 # fi
 
-# # Set a delay to wait to start meteor container
-# if [[ $DELAY ]]; then
-#   echo "Delaying startup for $DELAY seconds"
-#   sleep $DELAY
-# fi
+# Set a delay to wait to start meteor container
+if [[ $DELAY ]]; then
+  echo "Delaying startup for $DELAY seconds"
+  sleep $DELAY
+fi
 
 # Honour already existing PORT setup
 export PORT=${PORT:-80}
 
+cd /bundle/bundle
 echo "=> Starting meteor app on port:$PORT"
-node /bundle/bundle/main.js
+node main.js
